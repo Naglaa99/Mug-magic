463ae34735d748a3d3b03e5b39f86eb80abed09b
Naglaa99 <Naglaahamdi6@gmail.com>
2023-09-15 04:48:27 +0300

    create coffee shop by html , css . javascript

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+/* ==========  Load  ========== */
+onload = ()=> {
+  const load = document.getElementById("load");
+  setTimeout(() => {
+    load.style.display = "none";
+  },3000);
+};
+  var dropdownBtn = document.querySelector('.dropdown-btn');
+  var dropdownMenu = document.querySelector('.dropdown-menu');
+
+  dropdownBtn.addEventListener('click', function() {
+    if (dropdownMenu.style.display === 'block') {
+      dropdownMenu.style.display = 'none';
+    } else {
+      dropdownMenu.style.display = 'block';
+    }
+  });
